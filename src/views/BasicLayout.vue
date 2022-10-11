b56d61b0b98a176e5525738b6bdd6bcc92590210
harjim <zhouping.h.app@outlook.com>
2022-10-11 09:05:04 +0800

    build: 引入 varlet UI 库及配置自动按需导入

--- src/views/BasicLayout.vue
+++ src/views/BasicLayout.vue
@@ -1,5 +1,6 @@
 <template>
   <div>BasicLayout</div>
+  <var-button type="primary">主要按钮</var-button>
 </template>
 
 <script lang="ts" setup></script>
--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@
 import { defineConfig, loadEnv } from 'vite'
 import { ImportMetaEnv } from './src/types/env'
 import vue from '@vitejs/plugin-vue'
-import Jsx from '@vitejs/plugin-vue-jsx'
+import jsx from '@vitejs/plugin-vue-jsx'
 import { resolve } from 'path'
+import components from 'unplugin-vue-components/vite'
+import { VarletUIResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -13,7 +15,14 @@ export default defineConfig(({ mode }) => {
   return {
     base: env.VITE_BASE,
     envPrefix: ['VITE_', 'APP_'],
-    plugins: [vue(), Jsx()],
+    plugins: [
+      vue(),
+      jsx(),
+      components({
+        resolvers: [VarletUIResolver()],
+        dts: 'src/types/components.d.ts'
+      })
+    ],
     resolve: {
       alias: {
         '@': resolve(__dirname, 'src')
